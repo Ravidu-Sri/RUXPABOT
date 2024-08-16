1b87aec374aa5f6c98a272cb9eecae4a98655305
Ravidu-Sri <ravidusaranga04@gmail.com>
2024-08-16 15:02:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const port = process.env.PORT || 8000;
 //=============================================
 
 async function connectToWA() {
-console.log("Connecting pmbot 😶🚫...");
+console.log("Connecting RUXPABOT 😶🚫...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
 var { version } = await fetchLatestBaileysVersion()
 
