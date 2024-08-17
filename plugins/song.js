db2d7ed04b6b7f8c25309e84cacc3c2b2185486b
Ravidu-Sri <ravidusaranga04@gmail.com>
2024-08-17 23:17:01 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -1,1 +1,50 @@
+const {cmd , commands} = require('../command')
+const fg = require('api-dylux')
+const yt = require('yt-search')
+
+
+cmd({
+    pattern: "song",
+    desc: "download songs",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("sorry Please give me url or title")
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url
+
+let desc = `
+🎷 *RUXPABOT YOUTUBE SONG DOWNLOAD* 🎷
+
+titel: ${data.titel}
+description: ${data.description}
+time: ${data.timestamp}
+ago: ${data.ago}
+views: ${data.views}
+
+MADE BY PODDA MODz 😗🌹
+`
+await conn.sendMessage(from,{imge:{url: data.thumbnail},caption:desc},{quoted:mek});
+
+//download audio
+
+let down = await fg.yta(url)
+let downloadUrl = down.di_url
+
+//send audio message
+await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek});
+  
+
+
+
+    
+}catch(e){
+conosle.log(e)
+reply(`${e}`)
+}
+ })
+  
 
