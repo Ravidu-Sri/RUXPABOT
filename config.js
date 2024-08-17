6df1e929171c6e4bff80f12aa1373a9caa1ec69a
Ravidu-Sri <ravidusaranga04@gmail.com>
2024-08-17 23:23:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID, 
 ALIVE_IMG: process.env.AILVE_IMG || "https://telegra.ph/file/4d69c1e11007840a8c048.jpg",   
-AILVE_MSG: process.env.AILVE_MSG || "Holle",
+AILVE_MSG: process.env.AILVE_MSG || "Holle jjj",
 };
