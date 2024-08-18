6d187ac0f2d8f949b44f70a7a15ce3d063e7508d
Ravidu-Sri <ravidusaranga04@gmail.com>
2024-08-18 15:52:02 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,5 +1,6 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
+const { fetchJson } = require('../lib/functions')
 
 cmd({
     pattern: "ai",
@@ -9,3 +10,10 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+let data = await fetchJson(``)
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+    
