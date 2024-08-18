252f6521e3b07fec449d93492a7cfeea9e00e055
Ravidu-Sri <ravidusaranga04@gmail.com>
2024-08-18 14:21:39 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -11,7 +11,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if(!q) return reply("*Please give me url or title 😑...*")
+if(!q) return reply("*Please give me url or title 🎷😑...*")
 const search = await yts(q)
 const data = search.videos[0];
 const url = data.url
