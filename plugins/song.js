fe16c07d0092fa1d1f77457fd8db477ccea37e75
Ravidu-Sri <ravidusaranga04@gmail.com>
2024-08-18 14:16:33 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -32,7 +32,7 @@ await conn.sendMessage(from,{imge:{url: data.thumbnail},caption:desc},{quoted:me
 //download audio
 
 let down = await fg.yta(url)
-let downloadUrl = down.di_url
+let downloadUrl = down.dl_url
 
 //send audio message
 await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek});
