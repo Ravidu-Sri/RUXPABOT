fa2d1ff6a3735d1b7f1022f1441934f3d520d167
Podda Modz <ravidusaranga04@gmail.com>
2024-08-20 22:30:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -134,6 +134,13 @@ conn.sendFileUrl = async (jid, url, caption, quoted, options = {}) => {
               }
             }
 
+//============================================================================ 
+if(!isOwner && config.MODE === "private") return
+if(!isOwner && isGroup && config.MODE === "inbox") return   
+if(!isOwner && !isGroup && config.MODE === "groups") return   
+//============================================================================
+        
+
 
 const events = require('./command')
 const cmdName = isCmd ? body.slice(1).trim().split(" ")[0].toLowerCase() : false;
@@ -165,12 +172,7 @@ mek.type === "stickerMessage"
 ) {
 command.function(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply})
 }});
-//============================================================================ 
-if(!isOwner && config.MODE === "private") return
-if(!isOwner && isGroup && config.MODE === "inbox") return   
-if(!isOwner && !isGroup && config.MODE === "groups") return   
-//============================================================================
-        
+
 })
 }
 app.get("/", (req, res) => {
