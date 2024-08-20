e284bc977a4402ffecc6fc5ed564d5cce7c2db82
Podda Modz <ravidusaranga04@gmail.com>
2024-08-21 00:44:54 +0530

    Update mediafire.js

--- plugins/mediafire.js
+++ plugins/mediafire.js
@@ -2,17 +2,17 @@ const { mediafire, bot, isUrl } = require('../lib/index')
 
 bot(
 	{
-		pattern: 'mediafire ?(.*)',
+		pattern: "mediafire",
 		fromMe: true,
 		desc: 'Download mediafire file',
-		type: 'download',
+		type: "download",
 	},
 	async (message, match) => {
 		match = isUrl(match || message.reply_message.text)
-		if (!match) return await message.send('_Example : mediafire url_')
+		if (!match) return await message.send("_Example" : "mediafire url_")
 		const result = await mediafire(match)
 		if (!result)
-			return await message.send('*Not found*', {
+			return await message.send("*Not found*", {
 				quoted: message.quoted,
 			})
 		return await message.sendFromUrl(result)
