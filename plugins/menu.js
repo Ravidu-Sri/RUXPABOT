ac22335a76d7f86d971f54b88df917b4f50f89ab
Podda Modz <ravidusaranga04@gmail.com>
2024-08-22 13:45:08 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -9,3 +9,52 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+let menu = {
+main: '',
+download: '',
+group: '',
+owner: '',
+convert: '',
+search: '',
+}:
+
+for (let i = 0; i < commands.length; i++) {
+if (commands[i].pattern && !commands[i].dontAddCommandList) {
+menu[commands[i].category] += `.${commands[i].pattern}\n`;
+ }
+}   
+
+let madeMenu = `👋 *Hi ${pushname}*
+> *DOWNLOAD COMMANDS* ⬇️|👻
+${menu.download}
+
+
+> *MAIN COMMANDS* 💙|🧸
+${menu.main}
+
+
+> *GROUP COMMANDS* 📢|🦊
+${menu.group}
+
+
+> *OWNER COMMANDS* 👩‍💻|🔊
+${menu.owner}
+
+
+> *CONVERT COMMANDS* 👋|💙
+${memu.convert}
+
+
+> *SEARCH COMMANDS* 🔎|👩‍💻
+${menu.search@}
+
+
+*CREATOR BY PODDA MODz 💔👻*
+`
+await conn.sendMessage(from,{text:madeMenu},{quoted:mek})
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})    
