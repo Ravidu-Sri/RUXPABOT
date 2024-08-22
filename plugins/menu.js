7a8f85fdfaff4e2e3c442c2b1305389aad46cb49
Podda Modz <ravidusaranga04@gmail.com>
2024-08-22 13:04:38 +0530

    Create menu.js

---
+++ plugins/menu.js
@@ -0,0 +1,11 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "menu",
+    desc: "bot features menu.",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
