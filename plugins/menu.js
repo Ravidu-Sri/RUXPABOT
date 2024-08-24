a68e77af8fe080e43702d4af1e0c1740877b6514
Podda Modz <ravidusaranga04@gmail.com>
2024-08-24 07:03:08 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -9,6 +9,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+const config = await readEnv();  
 let menu = {
 main: '',
 download: '',
@@ -20,7 +21,7 @@ search: ''
 
 for (let i = 0; i < commands.length; i++) {
 if (commands[i].pattern && !commands[i].dontAddCommandList) {
-menu[commands[i].category] += `.${commands[i].pattern}\n`;
+menu[commands[i].category] += `${config.PREFIX}${commands[i].pattern}\n`;
  }
 }   
     
