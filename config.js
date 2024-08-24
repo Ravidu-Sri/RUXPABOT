aa83a301a0857920ecbe767ee7f3210fd20032d9
Podda Modz <ravidusaranga04@gmail.com>
2024-08-24 07:16:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID, 
-ALIVE_IMG: process.env.AILVE_IMG || "https://telegra.ph/file/0aa53d084024368c3bf14.jpg",   
-AILVE_MSG: process.env.AILVE_MSG || "Holle",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",   
+SESSION_ID: process.env.SESSION_ID,
+MONGODB: process.env.MONGODB || "Hi mongodb bot",
 };
