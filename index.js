d4075a0a663d645f47354f5ecc8cdf53e36d79a5
Podda Modz <ravidusaranga04@gmail.com>
2024-08-24 06:53:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ const util = require('util')
 const { sms,downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
-const prefix = '.'
 
 const ownerNumber = ['94723423602']
 
@@ -39,6 +38,14 @@ const port = process.env.PORT || 8000;
 //=============================================
 
 async function connectToWA() {
+//coconnect mongodb
+const connectDB = require('.lib/mongodb')
+connectDB();
+//============================================
+const {readEnv} = require('.lib/database')
+const config = await readEnv();
+const prefix = config.PREFIX
+//=============================================
 console.log("Connecting RUXPABOT 😶🚫...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
 var { version } = await fetchLatestBaileysVersion()
