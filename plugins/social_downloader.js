6c44fab30bebf7b3bdae7c814f232a9d46812a70
Podda Modz <ravidusaranga04@gmail.com>
2024-08-26 00:16:13 +0530

    Update social_downloader.js

--- plugins/social_downloader.js
+++ plugins/social_downloader.js
@@ -105,5 +105,26 @@ async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, send
 })
 
 
-
 //----- *Share with Others* ------//
+
+cmd({
+    pattern: "tiktok",
+    alias: ["tiktok"],
+    desc: "download tw videos",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        if (!q && !q.startsWith("https://")) return reply("give me tiktok url")
+        //fetch data from api  
+        let data = await fetchJson(`${baseUrl}/api/tiktok?url=${q}`)
+        reply("*Downloading...*")
+        //send video (hd,sd)
+        await conn.sendMessage(from, { video: { url: data.data.data.HD }, mimetype: "video/mp4", caption: `- QUALITY HD\n\n> ${cap}` }, { quoted: mek })
+        await conn.sendMessage(from, { video: { url: data.data.data.SD }, mimetype: "video/mp4", caption: `- QUALITY SD \n\n> ${cap}` }, { quoted: mek })  
+    } catch (e) {
+        console.log(e)
+        reply(`${e}`)
+    }
+})
