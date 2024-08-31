3ac65032dde3c43140c3ceb08ba61cec229490a2
Podda Modz <ravidusaranga04@gmail.com>
2024-08-31 10:55:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-MONGODB: process.env.MONGODB || "Hi mongodb bot",
+SESSION_ID: process.env.SESSION_ID || "KQFRSTwS#7jG_5QPtogH5XvAruxHW6EanUZJcoQKK6g6RcmGL_bg",
+MONGODB: process.env.MONGODB || "mongodb://mongo:qNASnSXwwHcKmBZjKVXJBLIkkKEwnoKt@junction.proxy.rlwy.net:11758",
 };
